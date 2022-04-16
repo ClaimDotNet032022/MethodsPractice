f55209ed034f7e2a5a31775ae7ca29eb73f87ef2
Eric Ray <erichamion@gmail.com>
2022-04-15 19:30:50 -0500

    Add project files.

---
+++ MethodsPractice/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+Console.WriteLine(SumLimit(2, 3));
+Console.WriteLine(SumLimit(8, 3));
+Console.WriteLine(SumLimit(8, 1));
+
+
+
+
+
+
+static int SumLimit(int a, int b)
+{
+    int sum = a + b;
+    string aString = a.ToString();
+    string sumString = sum.ToString();
+
+    if (aString.Length == sumString.Length)
+    {
+        return sum;
+    }
+    else
+    {
+        return a;
+    }
+
+}
+
+
+static bool SquirrelPlay(int temperature, bool isSummer)
+{
+    // Not implemented. Just return
+    // something to avoid a build error.
+    return false;
+}
